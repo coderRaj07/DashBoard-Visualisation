f3b59944e83024de005d6d87d1650ff2404b844e
coderRaj07 <12131501.edu@gmail.com>
2023-08-04 12:52:37 +0530

    FEAT_3_BE: filters added

--- backend/server.js
+++ backend/server.js
@@ -18,18 +18,31 @@ mongoose.connect('mongodb://localhost:27017/dashboarddb', {
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.get('/api/data', async (req, res) => {
   try {
-    const data = await DashboardData.find({});
+    const filters = req.query; // Get the query parameters from the request
+
+    // Build the query based on the filters
+    const query = {};
+    if (filters.end_year) query.end_year = filters.end_year;
+    if (filters.topic) query.topic = filters.topic;
+    if (filters.sector) query.sector = filters.sector;
+    if (filters.region) query.region = filters.region;
+    if (filters.pestle) query.pestle = filters.pestle;
+    if (filters.source) query.source = filters.source;
+    if (filters.swot) query.swot = filters.swot;
+    if (filters.country) query.country = filters.country;
+    if (filters.city) query.city = filters.city;
+
+    // Fetch data from MongoDB based on the filters
+    const data = await DashboardData.find(query);
+
     res.json(data);
-    console.log(data);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal server error' });
   }
-});
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app; // Export the app for use in model.js
