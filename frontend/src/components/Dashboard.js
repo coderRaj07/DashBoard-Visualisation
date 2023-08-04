b141793eec3b86c1079782a4c36dea5b60ebafe3
coderRaj07 <12131501.edu@gmail.com>
2023-08-04 13:22:14 +0530

    FEAT_4_FE: dashboard component changes

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,13 +1,13 @@
 // src/components/Dashboard.js
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchData, updateFilters } from '../store/dashboardSlice';
 import * as d3 from 'd3';
 
 const Dashboard = () => {
   const dispatch = useDispatch();
-  const { data, filters } = useSelector((state) => state.dashboard);
+  const { data } = useSelector((state) => state.dashboard);
   const svgRef = useRef(null);
 
   const [filterValues, setFilterValues] = useState({
@@ -22,49 +22,31 @@ const Dashboard = () => {
     city: '',
   });
 
-  useEffect(() => {
-    dispatch(fetchData(filterValues));
-  }, [dispatch, filterValues]);
+  const drawBarChart = useCallback(() => {
 
-  useEffect(() => {
-    if (data.length) {
-      drawBarChart();
-    }
-  }, [data]);
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFilterValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
-
-  const drawBarChart = () => {
-    // D3.js chart code...
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
     const width = 500 - margin.left - margin.right;
     const height = 300 - margin.top - margin.bottom;
-  
+
     const xScale = d3.scaleBand()
       .domain(data.map(d => d.title))
       .range([0, width])
       .padding(0.1);
-  
+
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.intensity)])
       .range([height, 0]);
-  
+
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
-  
+
     const svg = d3.select(svgRef.current);
-  
+
     svg.selectAll('*').remove();
-  
+
     const g = svg.append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
-  
+
     g.append('g')
       .attr('class', 'x-axis')
       .attr('transform', `translate(0,${height})`)
@@ -72,11 +54,11 @@ const Dashboard = () => {
       .selectAll('text')
       .style('text-anchor', 'end')
       .attr('transform', 'rotate(-45)');
-  
+
     g.append('g')
       .attr('class', 'y-axis')
       .call(yAxis);
-  
+
     g.selectAll('.bar')
       .data(data)
       .enter().append('rect')
@@ -86,8 +68,25 @@ const Dashboard = () => {
       .attr('width', xScale.bandwidth())
       .attr('height', d => height - yScale(d.intensity))
       .attr('fill', 'steelblue');
+  }, [data]);
+
+  useEffect(() => {
+    dispatch(fetchData(filterValues));
+  }, [dispatch, filterValues]);
+
+  useEffect(() => {
+    if (data.length) {
+      drawBarChart();
+    }
+  }, [data, drawBarChart]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFilterValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
-  
 
   return (
     <div>
@@ -109,3 +108,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
