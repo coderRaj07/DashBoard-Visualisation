e18bf32c01b204cc0355c578fb32677a5fdfd8ee
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2023-08-04 10:09:49 +0530

    FEAT_1: api updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const DashboardData = require('./schema');
 const app = express();
 const port = 5000;
 
@@ -18,7 +18,16 @@ mongoose.connect('mongodb://localhost:27017/dashboarddb', {
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+app.get('/api/data', async (req, res) => {
+  try {
+    const data = await DashboardData.find({});
+    res.json(data);
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
