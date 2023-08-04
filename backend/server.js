7c63aacba9bf7fae60c275fd32a96e49496b315b
coderRaj07 <12131501.edu@gmail.com>
2023-08-04 08:53:12 +0530

    FEAT_1: code Refactored for server.js

--- backend/server.js
+++ backend/server.js
@@ -19,35 +19,9 @@ mongoose.connect('mongodb://localhost:27017/dashboarddb', {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Define MongoDB schema and model (assuming the data structure)
-const dashboardDataSchema = new mongoose.Schema({
-  Intensity: String,
-  Likelihood: String,
-  Relevance: String,
-  Year: String,
-  Country: String,
-  Topics: String,
-  Region: String,
-  City: String,
-  // Other fields from the JSON data
-});
-
-const DashboardData = mongoose.model('DashboardData', dashboardDataSchema);
-
-// API to get data from MongoDB
-app.get('/api/data', (req, res) => {
-  DashboardData.find({}, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      res.json(data);
-    }
-  });
-});
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-module.exports = { DashboardData }; // Export the model for use in pushData.js
+module.exports = app; // Export the app for use in model.js
+
